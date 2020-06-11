62ffc8d050e462ebe2675f7d7a315e44f0c1d88e
AkIonSight <akionsight@gmail.com>
2020-06-11 23:27:25 +0530

    Delete speech_recognition.py

--- speech_recognition.py
+++
@@ -1,7 +0,0 @@
-import speech_recognition as sr
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print('Say Something:- ')
-    audio = r.listen(source) 
-    text = r.recognize_google(audio) 
-    print(text)
